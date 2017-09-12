fe9c74b9933745144bfd8134bc96fd77fa3291f8
Lusheng Ji <lji@research.att.com>
2017-09-12 02:16:34 +0000

    Add mvn script for docker build
    Issue-Id: DCAEGEN2-60
    Change-Id: Ic05eeb71ee94e29fcb2c0f2ca5a0ca4d36d9ca94
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -18,34 +18,86 @@
 #
 # ECOMP is a trademark and service mark of AT&T Intellectual Property.
 
+set -ex
+
 
 echo "running script: [$0] for module [$1] at stage [$2]"
 
-echo "=> Prepare environment "
-#env
+MVN_PROJECT_MODULEID="$1"
+MVN_PHASE="$2"
 
-TIMESTAMP=$(date +%C%y%m%dT%H%M%S) 
-export BUILD_NUMBER="${TIMESTAMP}"
 
-# expected environment variables 
+PROJECT_ROOT=$(dirname $0)
+
+FQDN="${MVN_PROJECT_GROUPID}.${MVN_PROJECT_ARTIFACTID}"
+if [ "$MVN_PROJECT_MODULEID" == "__" ]; then
+  MVN_PROJECT_MODULEID=""
+fi
+
+if [[ "$MVN_PROJECT_VERSION" == *SNAPSHOT ]]; then
+  echo "=> for SNAPSHOT artifact build"
+  MVN_DEPLOYMENT_TYPE='SNAPSHOT'
+else
+  echo "=> for STAGING/RELEASE artifact build"
+  MVN_DEPLOYMENT_TYPE='STAGING'
+fi
+echo "MVN_DEPLOYMENT_TYPE is             [$MVN_DEPLOYMENT_TYPE]"
+
+
+TIMESTAMP=$(date +%C%y%m%dT%H%M%S)
+
+# expected environment variables
 if [ -z "${MVN_NEXUSPROXY}" ]; then
     echo "MVN_NEXUSPROXY environment variable not set.  Cannot proceed"
     exit
 fi
 MVN_NEXUSPROXY_HOST=$(echo "$MVN_NEXUSPROXY" |cut -f3 -d'/' | cut -f1 -d':')
-echo "=> Nexu Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
+echo "=> Nexus Proxy at $MVN_NEXUSPROXY_HOST, $MVN_NEXUSPROXY"
 
-# use the version text detect which phase we are in in LF CICD process: verify, merge, or (daily) release
+if [ -z "$WORKSPACE" ]; then
+    WORKSPACE=$(pwd)
+fi
 
-# mvn phase in life cycle 
+if [ -z "$SETTINGS_FILE" ]; then
+    echo "SETTINGS_FILE environment variable not set.  Cannot proceed"
+    exit
+fi
+   
+
+
+# mvn phase in life cycle
 MVN_PHASE="$2"
 
+
+echo "MVN_PROJECT_MODULEID is            [$MVN_PROJECT_MODULEID]"
+echo "MVN_PHASE is                       [$MVN_PHASE]"
+echo "MVN_PROJECT_GROUPID is             [$MVN_PROJECT_GROUPID]"
+echo "MVN_PROJECT_ARTIFACTID is          [$MVN_PROJECT_ARTIFACTID]"
+echo "MVN_PROJECT_VERSION is             [$MVN_PROJECT_VERSION]"
+echo "MVN_NEXUSPROXY is                  [$MVN_NEXUSPROXY]"
+echo "MVN_RAWREPO_BASEURL_UPLOAD is      [$MVN_RAWREPO_BASEURL_UPLOAD]"
+echo "MVN_RAWREPO_BASEURL_DOWNLOAD is    [$MVN_RAWREPO_BASEURL_DOWNLOAD]"
+MVN_RAWREPO_HOST=$(echo "$MVN_RAWREPO_BASEURL_UPLOAD" | cut -f3 -d'/' |cut -f1 -d':')
+echo "MVN_RAWREPO_HOST is                [$MVN_RAWREPO_HOST]"
+echo "MVN_RAWREPO_SERVERID is            [$MVN_RAWREPO_SERVERID]"
+echo "MVN_DOCKERREGISTRY_DAILY is        [$MVN_DOCKERREGISTRY_DAILY]"
+echo "MVN_DOCKERREGISTRY_RELEASE is      [$MVN_DOCKERREGISTRY_RELEASE]"
+
+
+source "${PROJECT_ROOT}"/mvn-phase-lib.sh 
+
+
+# Customize the section below for each project
 case $MVN_PHASE in
 clean)
   echo "==> clean phase script"
+  clean_templated_files
+  clean_tox_files
+  rm -rf ./venv-* ./*.wgn ./site
   ;;
 generate-sources)
   echo "==> generate-sources phase script"
+  expand_templates
   ;;
 compile)
   echo "==> compile phase script"
@@ -61,6 +113,11 @@ install)
   ;;
 deploy)
   echo "==> deploy phase script"
+  # below segments are example of how to deploy various artifacts
+  # copy the ones suitable for your repo, and remove the "if false" statement
+
+  # build docker image from Docker file (under root of repo) and push to registry
+  build_and_push_docker
   ;;
 *)
   echo "==> unprocessed phase"
