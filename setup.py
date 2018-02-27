9c63d897d874d49d4de175a819f70b597c5509ab
Alex Shatov <alexs@att.com>
2018-02-27 12:00:00 -0500

    up the version to 2.1.0 for policy-handler
    Change-Id: I07f11e60c4677109ccb826c4e969b47acb4c498a
    Signed-off-by: Alex Shatov <alexs@att.com>
    Issue-ID: DCAEGEN2-347

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import setup
 setup(
     name='policyhandler',
     description='DCAE-Controller policy-handler to communicate with policy-engine',
-    version="2.0.0",
+    version="2.1.0",
     author='Alex Shatov',
     packages=['policyhandler'],
     zip_safe=False,
