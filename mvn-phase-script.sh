3c0002513e9f94e00a7ee4c8b43fe7405defbbc2
Lusheng Ji <lji@research.att.com>
2018-07-16 22:49:31 -0400

    Update release tag
    Issue-ID: DCAEGEN2-499
    Change-Id: Id7fc337a8c5b897cb26af1d10ed47d5718f48694
    Signed-off-by: Lusheng Ji <lji@research.att.com>

--- mvn-phase-script.sh
+++ mvn-phase-script.sh
@@ -37,7 +37,7 @@ if [ -z "$SETTINGS_FILE" ]; then
 fi
 
 set +e
-RELEASE_TAG=${MVN_RELEASE_TAG:-R2}
+RELEASE_TAG=${MVN_RELEASE_TAG:-R3}
 if [ "$RELEASE_TAG" != "R1" ]; then
   RELEASE_TAGGED_DIR="${RELEASE_TAG}/"
 else
