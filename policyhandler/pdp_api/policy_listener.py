e9d880915286c468df74d152c8ef9a56de1d4f2f
aviral1997 <aviral.thakral@wipro.com>
2019-12-19 12:56:49 +0530

    Modify new pdp_api to support DMaaP
    ADD DMaaP Library
    Modify Listener to receive DMaaP notification
    Update by Match only for policy_id and policy_version
    
    Issue-ID: DCAEGEN2-1851
    Signed-off-by: aviral1997 <aviral.thakral@wipro.com>
    Change-Id: Ie9fdb6061ae1c02281d9800cf37c2d934b5fc20c

--- policyhandler/pdp_api/policy_listener.py
+++ policyhandler/pdp_api/policy_listener.py
@@ -1,55 +1,285 @@
-# ================================================================================
-# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
-# ================================================================================
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============LICENSE_END=========================================================
+# ============LICENSE_START=======================================================
+ # policy-handler
+ #  ================================================================================
+ #  Copyright (C) 2019 Wipro Limited.
+ #  ==============================================================================
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+ #
+ #        http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+ #   ============LICENSE_END=========================================================
 #
 
 """
 policy-listener communicates with policy-engine
-to receive push notifications
+thru DMaap to receive push notifications
 on updates and removal of policies.
 
 on receiving the policy-notifications, the policy-receiver
 passes the notifications to policy-updater
 """
 
+import copy
+import json
 import os
+import requests as r
+import time
+import urllib.parse
+from datetime import datetime
+from threading import Lock, Thread
 
-from ..utils import ToBeImplementedException, Utils
+from .DMaap import Subscribe
+
+from ..config import Config, Settings
+from ..onap.audit import Audit
+from ..utils import Utils
+from .pdp_consts import POLICY_NAME, POLICY_VERSION, PDP_POLICY_ID, PDP_POLICY_VERSION
+
+REMOVED = 'deleted-policies'
+ADDED = 'added-policies'
+ID = 'policy-id'
+POLICY_VER = 'policy-version'
+
 
 _LOGGER = Utils.get_logger(__file__)
 
-class PolicyListener(object):
-    """listener to PolicyEngine"""
+class PolicyListener(Thread):
+    """DMaap to PolicyEngine"""
     PDP_API_FOLDER = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    DMAAP_HEALTH = "DMaap_health"
+    DMAAP_MESSAGE_COUNT = "message_count"
+    DMAAP_ERROR_COUNT = "error_count"
+    DMAAP_MESSAGE_TIMESTAMP = "message_timestamp"
+    DMAAP_STATUS = "dmaap_status"
 
-    def __init__(self, *_):
-        """listener to receive the policy notifications from PolicyEngine"""
-        _LOGGER.info("to_be_implemented")
-        raise ToBeImplementedException()
+    def __init__(self, audit, policy_updater):
+        """DMaap inside the thread to receive policy notifications from PolicyEngine"""
+        Thread.__init__(self, name="policy_receiver", daemon=True)
+        self._policy_updater = policy_updater
+        self._lock = Lock()
+        self._settings = Settings(Config.FIELD_DMAAP)
+        self._keep_running = True
+        self._sleep_before_restarting = 5
+        self.server_info=None
+        self.group=None
+        self.group_id=None
+        self.topic=None
+        self._dmaap_health = {
+            PolicyListener.DMAAP_MESSAGE_COUNT : 0
+        }
+        Audit.register_item_health(PolicyListener.DMAAP_HEALTH, self._get_health)
+        self.reconfigure(audit)
 
-    def reconfigure(self, _):
-        """configure and reconfigure the listener"""
-        _LOGGER.info("to_be_implemented")
-        raise ToBeImplementedException()
+    def reconfigure(self,audit):
+            """configure and reconfigure the DMaap"""
+            with self._lock:
+                _LOGGER.info(audit.info("DMaap_HEALTH {}".format(
+                    json.dumps(self._get_health(), sort_keys=True))))
+                self._sleep_before_restarting = 5
+                self._settings.set_config(Config.discovered_config)
+                changed, config = self._settings.get_by_key(Config.FIELD_DMAAP)
+
+                if not changed:
+                    self._settings.commit_change()
+
+                prev_dmaap_server_info = self.server_info
+                prev_dmaap_topic = self.topic
+                prev_group = self.group
+                prev_group_id = self.group_id
+
+                self.server_info=config.get("server_info")
+                self.group=config.get("group")
+                self.group_id=config.get("group_id")
+                self.topic=config.get("topic")
+
+                log_changed = (
+                    "changed dmaap_server_info(%s) or dmaap_topic(%s)"
+                    " or dmaap_group(%s) or dmaap_group_id(%s): %s" %
+                    (self.server_info, self.topic, self.group, self.group_id,
+                     self._settings))
+
+                if (self.server_info == prev_dmaap_server_info
+                        and self.topic == prev_dmaap_topic
+                        and self.group == prev_group
+                        and self.group_id == prev_group_id):
+                    _LOGGER.info(audit.info("not {}".format(log_changed)))
+                    self._settings.commit_change()
+                    return False
+
+                _LOGGER.info(audit.info(log_changed))
+                self._settings.commit_change()
+
+            self._stop_notifications()
+            return True
 
     def run(self):
-        """listen on web-socket and pass the policy notifications to policy-updater"""
-        _LOGGER.info("to_be_implemented")
-        raise ToBeImplementedException()
+        """subscribe to dmaap and pass the policy notifications to policy-updater"""
+        _LOGGER.info("starting policy_receiver...")
+        restarting = False
+        while True:
+            if self._get_keep_running():
+                break
 
-    def shutdown(self, _):
+            self._keep_running = True
+            if restarting:
+                with self._lock:
+                    sleep_before_restarting = self._sleep_before_restarting
+                _LOGGER.info(
+                    "going to sleep for %s secs before restarting policy-notifications",
+                    sleep_before_restarting)
+
+                time.sleep(sleep_before_restarting)
+                if not self._get_keep_running():
+                    break
+
+            dmaap_obj=Subscribe()
+
+            with self._lock:
+                dmaap_obj.server_info = self.server_info
+                dmaap_obj.topic = self.topic
+                dmaap_obj.group = self.group
+                dmaap_obj.group_id = self.group_id
+
+
+            _LOGGER.info(
+                "connecting to policy-notifications at %s with topic(%s) group(%s) and group id (%s)",
+                server_info, topic, group, group_id)
+            _LOGGER.info("waiting for policy-notifications...")
+
+            while(self._get_keep_running()):
+                try:
+                    message,res_status=dmaap_obj.receive()
+                    if res_status == r.codes.ok:
+                        self._dmaap_health[PolicyListener.DMAAP_STATUS]= "connected"
+                        if message=="" or message == None:
+                            continue
+                        else:
+                            self._on_pdp_message(message)
+                    else:
+                        self._dmaap_health[PolicyListener.DMAAP_STATUS]= "disconnected"
+                except OSError as e:
+                    error = str(e)
+                    self._on_error(error)
+
+                except DMaaPError as e:
+                    error = str(e)
+                    self._on_error(error)
+
+            restarting = True
+
+        Audit.register_item_health(PolicyListener.DMAAP_HEALTH)
+        _LOGGER.info("exit policy-receiver")
+
+    def _get_keep_running(self):
+        with self._lock:
+            keep_running = self._keep_running
+        return keep_running
+
+    def _stop_notifications(self):
+        """close the dmaap == stops the notification service if running."""
+        with self._lock:
+            if self._keep_running:
+                self._keep_running = False
+                _LOGGER.info("stopped receiving notifications from PDP")
+
+
+    def _on_pdp_message(self, *args):
+        """received the notification from PDP
+
+            added-policies:
+                -
+                    policy-type: onap.policies.monitoring.cdap.tca.hi.lo.app
+                    policy-type-version: 1.0.0
+                    policy-id: onap.scaleout.tca
+                    policy-version: 2.0.0
+                    success-count: 3
+                    failure-count: 0
+                    incomplete-count: 0
+            deleted-policies:
+            -
+                    policy-type: onap.policies.monitoring.cdap.tca.hi.lo.app
+                    policy-type-version: 1.0.0
+                    policy-id: onap.scaleout.tca
+                    policy-version: 1.0.0
+                    success-count: 3
+                    failure-count: 0
+                    incomplete-count: 0
+
+
+        """
+        self._dmaap_health[PolicyListener.DMAAP_MESSAGE_COUNT] += 1
+        self._dmaap_health[PolicyListener.DMAAP_MESSAGE_TIMESTAMP] = str(datetime.utcnow())
+        try:
+            message = args and args[-1]
+            _LOGGER.info("Received notification message: %s", message)
+            _LOGGER.info("dmaap_health %s", json.dumps(self._get_health(), sort_keys=True))
+            if not message:
+                return
+            message = json.loads(message)
+
+            if not message or not isinstance(message, dict):
+                _LOGGER.warning("unexpected message from PDP: %s", json.dumps(message))
+                return
+            if message.get(ADDED):
+                message = message.get(ADDED)
+            policies_updated = [
+                {PDP_POLICY_ID: message.get(ID),
+                 POLICY_VERSION: message.get(POLICY_VER)}
+
+            ]
+
+            if message.get(REMOVED):
+                message=message.get(REMOVED)
+
+            policies_removed = [
+                {PDP_POLICY_ID: message.get(ID),
+                 PDP_POLICY_VERSION: message.get(POLICY_VER)}
+
+            ]
+
+            if not policies_updated and not policies_removed:
+                _LOGGER.info("no policy updated or removed")
+                return
+
+            self._policy_updater.policy_update(policies_updated, policies_removed)
+        except Exception as ex:
+            error_msg = "crash {} {} at {}: {}".format(type(ex).__name__, str(ex),
+                                                       "on_pdp_message", json.dumps(message))
+
+            _LOGGER.exception(error_msg)
+
+
+
+    def _get_health(self):
+        """returns the healthcheck of the dmaap as json"""
+        dmaap_health = copy.deepcopy(self._dmaap_health)
+        return dmaap_health
+
+
+    def shutdown(self, audit):
         """Shutdown the policy-listener"""
-        _LOGGER.info("to_be_implemented")
-        raise ToBeImplementedException()
+        _LOGGER.info(audit.info("shutdown policy-listener"))
+        with self._lock:
+            self._keep_running = False
+
+        self._stop_notifications()
+
+        if self.is_alive():
+            self.join()
+
+    def _on_error(self,error):
+        _LOGGER.exception("policy-notification error %s", str(error))
+        self._sleep_before_restarting = 60 if isinstance(error, ssl.SSLError) else 5
+        self._dmaap_health[PolicyListener.DMAAP_STATUS] = "error"
+        self._dmaap_health[PolicyListener.DMAAP_ERROR_COUNT] += 1
+        self._dmaap_health["last_error"] = {
+            "error": str(error), "timestamp": str(datetime.utcnow())
+        }
+        _LOGGER.info("dmaap_health %s", json.dumps(self._get_health(), sort_keys=True))
--- policyhandler/pdp_api/policy_matcher.py
+++ policyhandler/pdp_api/policy_matcher.py
@@ -1,25 +1,183 @@
-# ================================================================================
-# Copyright (c) 2019 AT&T Intellectual Property. All rights reserved.
-# ================================================================================
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ============LICENSE_END=========================================================
+# ============LICENSE_START=======================================================
+ # policy-handler
+ #  ================================================================================
+ #  Copyright (C) 2019 Wipro Limited.
+ #  ==============================================================================
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+ #
+ #        http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+ #   ============LICENSE_END=========================================================
 #
 
 """policy-matcher matches the policies from deployment-handler to policies from policy-engine"""
-
+import json
 import os
+import re
+
+from ..deploy_handler import DeployHandler, PolicyUpdateMessage
+from ..onap.audit import AuditHttpCode, AuditResponseCode
+from ..policy_consts import (ERRORED_POLICIES, LATEST_POLICIES, POLICY_BODY,
+                             POLICY_FILTER, POLICY_VERSIONS)
+from ..utils import RegexCoarser, Utils
+from .pdp_consts import POLICY_NAME, POLICY_VERSION
+from .policy_rest import PolicyRest
 
 
 class PolicyMatcher(object):
     """policy-matcher - static class"""
+    PENDING_UPDATE = "pending_update"
     PDP_API_FOLDER = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+
+    @staticmethod
+    def get_deployed_policies(audit):
+        """get the deployed policies and policy-filters"""
+        deployed_policies, deployed_policy_filters = DeployHandler.get_deployed_policies(audit)
+
+        if audit.is_not_found():
+            warning_txt = "got no deployed policies or policy-filters"
+            _LOGGER.warning(warning_txt)
+            return {"warning": warning_txt}, None, None
+
+        if not audit.is_success() or (not deployed_policies and not deployed_policy_filters):
+            error_txt = "failed to retrieve policies from deployment-handler"
+            _LOGGER.error(error_txt)
+            return {"error": error_txt}, None, None
+
+        return None, deployed_policies, deployed_policy_filters
+
+
+    @staticmethod
+    def build_catch_up_message(audit, deployed_policies, deployed_policy_filters):
+        """
+        find the latest policies from policy-engine for the deployed policies and policy-filters
+        """
+
+        if not (deployed_policies or deployed_policy_filters):
+            error_txt = "no deployed policies or policy-filters"
+            _LOGGER.warning(error_txt)
+            return {"error": error_txt}, None
+
+        coarse_regex_patterns = PolicyMatcher.calc_coarse_patterns(
+            audit, deployed_policies, deployed_policy_filters)
+
+        if not coarse_regex_patterns:
+            error_txt = ("failed to construct the coarse_regex_patterns from " +
+                         "deployed_policies: {} and deployed_policy_filters: {}"
+                         .format(deployed_policies, deployed_policy_filters))
+            _LOGGER.error(audit.error(
+                error_txt, error_code=AuditResponseCode.DATA_ERROR))
+            audit.set_http_status_code(AuditHttpCode.DATA_ERROR.value)
+            return {"error": error_txt}, None
+
+        pdp_response = PolicyRest.get_latest_policies(
+            audit, policy_filters=[{POLICY_NAME: policy_name_pattern}
+                                   for policy_name_pattern in coarse_regex_patterns]
+        )
+
+        if not audit.is_success():
+            error_txt = "failed to retrieve policies from policy-engine"
+            _LOGGER.warning(error_txt)
+            return {"error": error_txt}, None
+
+        latest_policies = pdp_response.get(LATEST_POLICIES, {})
+        errored_policies = pdp_response.get(ERRORED_POLICIES, {})
+
+        latest_policies, changed_policies, policy_filter_matches = PolicyMatcher._match_policies(
+            audit, latest_policies, deployed_policies, deployed_policy_filters)
+
+        errored_policies = dict((policy_id, policy)
+                                for (policy_id, policy) in errored_policies.items()
+                                if deployed_policies.get(policy_id, {}).get(POLICY_VERSIONS))
+
+        removed_policies = dict(
+            (policy_id, True)
+            for (policy_id, deployed_policy) in deployed_policies.items()
+            if deployed_policy.get(POLICY_VERSIONS)
+            and policy_id not in latest_policies
+            and policy_id not in errored_policies
+        )
+
+        return ({LATEST_POLICIES: latest_policies, ERRORED_POLICIES: errored_policies},
+                PolicyUpdateMessage(changed_policies,
+                                    removed_policies,
+                                    policy_filter_matches))
+
+    @staticmethod
+    def calc_coarse_patterns(audit, deployed_policies, deployed_policy_filters):
+        """calculate the coarsed patterns on policy-names in policies and policy-filters"""
+        coarse_regex = RegexCoarser()
+        for policy_id in deployed_policies or {}:
+            coarse_regex.add_regex_pattern(policy_id)
+
+        for policy_filter in (deployed_policy_filters or {}).values():
+            policy_name_pattern = policy_filter.get(POLICY_FILTER, {}).get(POLICY_NAME)
+            coarse_regex.add_regex_pattern(policy_name_pattern)
+
+        coarse_regex_patterns = coarse_regex.get_coarse_regex_patterns()
+        _LOGGER.debug(
+            audit.debug("coarse_regex_patterns({}) combined_regex_pattern({}) for patterns({})"
+                        .format(coarse_regex_patterns,
+                                coarse_regex.get_combined_regex_pattern(),
+                                coarse_regex.patterns)))
+        return coarse_regex_patterns
+
+
+    @staticmethod
+    def match_to_deployed_policies(audit, policies_updated, policies_removed):
+        """match the policies_updated, policies_removed versus deployed policies"""
+        deployed_policies, deployed_policy_filters = DeployHandler.get_deployed_policies(audit)
+        if not audit.is_success():
+            return {}, {}, {}
+
+        _, changed_policies = PolicyMatcher._match_policies(
+            audit, policies_updated, deployed_policies)
+
+        policies_removed = dict((policy_id, policy)
+                                for (policy_id, policy) in policies_removed.items()
+                                if deployed_policies.get(policy_id, {}).get(POLICY_VERSIONS))
+        return changed_policies, policies_removed
+
+
+
+    @staticmethod
+    def _match_policies(audit, policies, deployed_policies):
+        """
+        Match policies to deployed policies by policy_id.
+
+        Also calculates the policies that changed in comparison to deployed policies
+        """
+        matching_policies = {}
+        changed_policies = {}
+
+
+        policies = policies or {}
+        deployed_policies = deployed_policies or {}
+
+
+        for (policy_id, policy) in policies.items():
+            if type(policy) is str:
+                continue
+            else :
+                new_version = policy.get(POLICY_VERSION)
+                deployed_policy = deployed_policies.get(policy_id)
+
+                if deployed_policy:
+                    matching_policies[policy_id] = policy
+
+                policy_changed = (deployed_policy and new_version
+                                  and (deployed_policy.get(PolicyMatcher.PENDING_UPDATE)
+                                       or {new_version} ^
+                                       deployed_policy.get(POLICY_VERSIONS, {}).keys()))
+                if policy_changed:
+                    changed_policies[policy_id] = policy
+
+
+        return matching_policies, changed_policies
