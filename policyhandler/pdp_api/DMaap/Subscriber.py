535163784f9efbd05d7fc5e06e216516ac286627
aviral1997 <aviral.thakral@wipro.com>
2019-12-19 12:56:49 +0530

    Modify new pdp_api to support DMaaP
    ADD DMaaP Library
    Modify Listener to receive DMaaP notification
    Update by Match only for policy_id and policy_version
    
    Issue-ID: DCAEGEN2-1851
    Signed-off-by: aviral1997 <aviral.thakral@wipro.com>
    Change-Id: Ie9fdb6061ae1c02281d9800cf37c2d934b5fc20c

---
+++ policyhandler/pdp_api/DMaap/Subscriber.py
@@ -0,0 +1,71 @@
+# ============LICENSE_START=======================================================
+ # policy-handler
+ #  ================================================================================
+ #  Copyright (C) 2019 Wipro Limited.
+ #  ==============================================================================
+ #   Licensed under the Apache License, Version 2.0 (the "License");
+ #   you may not use this file except in compliance with the License.
+ #   You may obtain a copy of the License at
+ #
+ #        http://www.apache.org/licenses/LICENSE-2.0
+ #
+ #   Unless required by applicable law or agreed to in writing, software
+ #   distributed under the License is distributed on an "AS IS" BASIS,
+ #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ #   See the License for the specific language governing permissions and
+ #   limitations under the License.
+ #   ============LICENSE_END=========================================================
+
+
+import requests as r
+import threading
+import urllib
+from urllib.parse import urljoin
+
+class DMaaPError(Exception):
+	pass
+
+
+
+class Subscriber:
+	"""Subscribe to DMaap to listen for policy change message"""
+	def __init__(self):
+		self.server_info = ""
+		self.topic=""
+		self.group=""
+		self.group_id=""
+		self.dmaap_timeout=10
+		self.http_retries = 10
+		self.username=""
+		self.passwd=""
+
+
+	def receive(self):
+		http_headers = {"Accept": "application/json"}
+		retry_count=0
+		dmaap_sub_success=False
+		msg="Connection Error"
+		path= ("events/"+self.topic+"/"+self.group+"/"+self.group_id)
+		url = urljoin(self.server_info,path)
+		url=urllib.parse.quote_plus(url, safe='')
+		while not dmaap_sub_success and retry_count < (self.http_retries):
+			try:
+				if(self.username==""):
+					http_resp=r.get(url,http_headers,timeout=self.dmaap_timeout)
+					if http_resp.status_code == r.codes.ok:
+						resp= http_resp.json()
+
+						if not(resp is None or resp is ""):
+							return resp,http_resp.status_code
+					else:
+						return msg, http_resp.status_code
+
+			except OSError as e:
+				msg = "OS exception while attempting to post: %s" % (str(e))
+				raise OSError(msg)
+
+			retry_count+=1
+
+		if not dmaap_sub_success:
+		   mssg = "ALL subscribe attempts failed"
+		   raise DMaaPError(mssg)
